4a4f33fd6fc9dc39220a0e34258ab8048d80c766
maherreramu <64550002+maherreramu@users.noreply.github.com>
2022-02-04 16:41:51 -0500

    Update

--- Proyecto_final/nx_dijkstra.py
+++ Proyecto_final/nx_dijkstra.py
@@ -1,5 +1,19 @@
 import networkx as nx
-from graph import G
+from graph import G, cities
 
-path = nx.shortest_path(G, source='Cali', target='Bucaramanga', weight='dist')
-print(path)
+#st = nodo inicio
+#end = nodo final
+#mode = 'dist' o 'time'
+def nx_dijkstra(st, end, mode):
+    if mode == 'dist' or mode == 'time':
+        if cities.index(st) and cities.index(end):
+            path = nx.shortest_path(G, source=st, target=end, weight=mode)
+
+            data = 0
+            if len(path) < 3:
+                data = G.get_edge_data(cities[0], cities[1])[mode]
+            else:
+                for i in range(0, len(path)-1):
+                    G.get_edge_data(cities[i], cities[i+1])[mode]
+                    
+            return (path, data)
